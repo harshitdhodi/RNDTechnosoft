e885e77ee4275a2dd2c80015fe185c2338090134
harshitdhodi <harshit.dhodi2108@gmail.com>
2025-05-19 16:35:09 +0530

    add HexGridDemo at service page

--- backend/dist/assets/Faq-Bxfsb5m8-1747646010149.js
+++
@@ -1,1 +0,0 @@
-import{j as s,J as t}from"./index-CIp74flp-1747646010149.js";function r(){return s.jsx("div",{className:"mt-40",children:s.jsx(t,{})})}export{r as default};
---
+++ backend/dist/assets/Faq-oGyg-dAZ-1747652548551.js
@@ -0,0 +1,1 @@
+import{j as s,J as t}from"./index-BPYgVeg6-1747652548551.js";function r(){return s.jsx("div",{className:"mt-40",children:s.jsx(t,{})})}export{r as default};
