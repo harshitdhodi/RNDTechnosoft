e5001cdc798d8341f67a79c001226bf1547c1c34
harshitdhodi <harshit.dhodi2108@gmail.com>
2025-05-27 11:17:02 +0530

    new build for 3053 port

--- backend/dist/assets/Faq-78tWOmsu-1748000791857.js
+++
@@ -1,1 +0,0 @@
-import{j as s,J as t}from"./index-BUtzwP4G-1748000791857.js";function r(){return s.jsx("div",{className:"mt-40",children:s.jsx(t,{})})}export{r as default};
---
+++ backend/dist/assets/Faq-BQFUAieD-1748324620210.js
@@ -0,0 +1,1 @@
+import{j as s,J as t}from"./index-DdACMLMn-1748324620210.js";function r(){return s.jsx("div",{className:"mt-40",children:s.jsx(t,{})})}export{r as default};
