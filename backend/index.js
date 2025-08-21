b2a628fad235ebb5982a3ca3d9c789f5e7623d04
vrajeshrndtechnosoft <vrajeshpatel.rndtechnosoft@gmail.com>
2025-08-21 10:19:27 +0530

    updated with merge file

--- backend/index.js
+++ backend/index.js
@@ -480,7 +480,7 @@ app.use('/api/technology', require('./routes/technology'));
 app.use('/api/technologySecData', require('./routes/technologySecData'));
 app.use('/api/caseStudy', require('./routes/caseStudy'));
 app.use('/api/hire-talent', require('./routes/hire_telent'));
-app.use("/api/google", require("./routes/googleReviews.js"));
+// app.use("/api/google", require("./routes/googleReviews.js"));
 
 
 // Swagger Setup 
